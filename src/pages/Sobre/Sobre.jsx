279311bd1ce9c77bc50cf6d79df83b213d3bc534
joao.simao <vitoralves1604@gmail.com>
2024-03-14 16:09:21 -0300

    Fix: Fixed layout

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -7,9 +7,13 @@ export default function Sobre() {
             <Col className="colSobre">
                 <p className="sobre">Olá, Meu nome é João Vitor !
                     Sou extremamente apaixonado pela área de tecnologia, principalmente programação!
-                    Tive meu primeiro contato com o desenvolvimento aos 17 anos e desde então sigo em
-                    busca de melhorar a cada dia ! Além de programação, tenho um bom conhecimento de
-                    manutenção de computadores e redes.
+                    Iniciei meus estudos em 2015, onde através de um curso realizado na unidade do SENAI, obtive conhecimentos
+                    de manutenção de redes e computadores.
+                    Tive meu primeiro contato com o desenvolvimento em 2017 aos 17 anos, onde passei por linguagens como 
+                    <b> JAVA</b> e <b>PHP</b>, mas em 2022 tive meus primeiros contatos com <b>C#</b> e desde então sigo em
+                    busca de melhorar a cada dia ! Atualmente, além do <b>Backend</b> e <b>Banco de dados </b>
+                    estou estudando <b>Frontend </b> utilizando <b>ReactJS</b> e esta página foi construída no intuido de praticar um pouco.
+                    Confira abaixo algumas das tecnologias que já trabalhei e trabalho.
                 </p>
             </Col>
         </Row>
